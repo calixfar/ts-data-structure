40643c0af18ca1a81de1912a6a2b94551ea7a57a
luissantiago <luis.santiago@itglobers.com>
2022-08-26 15:03:29 -0500

    feat: tests for selectionSort algorithm

---
+++ src/algorithms/sort/selection-sort/selection-sort.spec.ts
@@ -0,0 +1,23 @@
+import { selectionSort, selectionSortV2 } from './selection-sort'
+
+function executeTestForSelectionSort (fn: (arr: number[]) => number[]) {
+  test('should returns array with the same item if the array prop has one item', () => {
+    const arr = [1]
+
+    expect(fn(arr)).toEqual(arr)
+  })
+
+  test('should returns ordered array', () => { 
+    const arr = [5,4,3,2,1]
+
+    expect(fn(arr)).toEqual(arr.reverse())
+   })
+}
+
+describe('Selection Sort Algorithm', () => {
+  executeTestForSelectionSort(selectionSort)
+}) 
+
+describe('Selection Sort Algorithm V2', () => {
+  executeTestForSelectionSort(selectionSortV2)
+}) 
